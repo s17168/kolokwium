28f9cd1abdad0f279e752164c4eb945f0498262d
s17168 <s17168@pjwstk.edu.pl>
2020-06-21 13:44:53 +0200

    Add first migration

---
+++ Kolokwium/Migrations/20200621114248_GenerateBaseSchemaMig.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kolokwium.Migrations
+{
+    public partial class GenerateBaseSchemaMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Musician",
+                columns: table => new
+                {
+                    IdMusician = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(maxLength: 30, nullable: true),
+                    LastName = table.Column<string>(maxLength: 50, nullable: true),
+                    NickName = table.Column<string>(maxLength: 20, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Musician", x => x.IdMusician);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MusicLabel",
+                columns: table => new
+                {
+                    IdMusicLabel = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MusicLabel", x => x.IdMusicLabel);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Album",
+                columns: table => new
+                {
+                    IdAlbum = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AlbumName = table.Column<string>(maxLength: 30, nullable: true),
+                    PublishDate = table.Column<DateTime>(nullable: false),
+                    IdMusicLabel = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Album", x => x.IdAlbum);
+                    table.ForeignKey(
+                        name: "FK_Album_MusicLabel_IdMusicLabel",
+                        column: x => x.IdMusicLabel,
+                        principalTable: "MusicLabel",
+                        principalColumn: "IdMusicLabel",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Track",
+                columns: table => new
+                {
+                    IdTrack = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TrackName = table.Column<string>(maxLength: 20, nullable: true),
+                    Duration = table.Column<float>(nullable: false),
+                    IdMusicAlbum = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Track", x => x.IdTrack);
+                    table.ForeignKey(
+                        name: "FK_Track_Album_IdMusicAlbum",
+                        column: x => x.IdMusicAlbum,
+                        principalTable: "Album",
+                        principalColumn: "IdAlbum",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MusicianTrack",
+                columns: table => new
+                {
+                    IdMusicianTrack = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdTrack = table.Column<int>(nullable: false),
+                    IdMusician = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MusicianTrack", x => x.IdMusicianTrack);
+                    table.ForeignKey(
+                        name: "FK_MusicianTrack_Musician_IdMusician",
+                        column: x => x.IdMusician,
+                        principalTable: "Musician",
+                        principalColumn: "IdMusician",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MusicianTrack_Track_IdTrack",
+                        column: x => x.IdTrack,
+                        principalTable: "Track",
+                        principalColumn: "IdTrack",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Album_IdMusicLabel",
+                table: "Album",
+                column: "IdMusicLabel");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MusicianTrack_IdMusician",
+                table: "MusicianTrack",
+                column: "IdMusician");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MusicianTrack_IdTrack",
+                table: "MusicianTrack",
+                column: "IdTrack");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Track_IdMusicAlbum",
+                table: "Track",
+                column: "IdMusicAlbum");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MusicianTrack");
+
+            migrationBuilder.DropTable(
+                name: "Musician");
+
+            migrationBuilder.DropTable(
+                name: "Track");
+
+            migrationBuilder.DropTable(
+                name: "Album");
+
+            migrationBuilder.DropTable(
+                name: "MusicLabel");
+        }
+    }
+}
